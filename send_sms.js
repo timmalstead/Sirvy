1c99ddb3ccc87543c4172032f102927fc00dbde2
Tim Malstead <timmmalstead@gmail.com>
2019-12-02 22:20:01 -0800

    hides personal info

--- send_sms.js
+++ send_sms.js
@@ -4,7 +4,7 @@ dotenv.config();
 const client = require('twilio')(process.env.TWILIO_SID,process.env.TWILIO_TOKEN)
 
 client.messages.create({
-    body : 'Tim, I think you found those local variables',
-    from : '+12132141135',
-    to : '+17604683754'
+    body : 'MESSAGE TO SEND',
+    from : 'SENDING NUMBER',
+    to : 'RECIEVING NUMBER'
 })
